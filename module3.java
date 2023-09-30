9479bd3b4b30d6e8c2b85a94cf3b8751c2efb31f
bipin-shrestha <iambpn.shrestha@gmail.com>
2023-09-30 14:07:18 -0700

    Updated Module 3 Critical Thinking Option #1

--- module3.java
+++ module3.java
@@ -14,9 +14,9 @@ public class module3 {
 		//Determine the tax rates on the base of income 
 		if(income < 500) {
 			taxrate = 0.10; // Tax rate is 10% for income less than 500
-		} else if (income < 1500) {
+		} else if (income < 1500 && income >= 500) {
 			taxrate = 0.15; // Tax rate is 15% for the income greater than/equal to $500 and less than $1500
-		} else if (income < 2500) {
+		} else if (income < 2500 && income >=1500) {
 			taxrate = 0.20; // Tax rate is 20% for the income greater than/equal to $1500 and less than $2500 
 		} else {
 			taxrate = 0.30; // Tax rate is 30% for the income greater than/equal to $2500
